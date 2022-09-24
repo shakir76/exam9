bf9c92bea12125f40081387f5311295a86aa8107
shakir <shakirmuhammetov@gmail.com>
2022-09-24 13:58:30 +0600

    Добавил редактирования и удаления

--- webapp/photo/photo.py
+++ webapp/photo/photo.py
@@ -1,7 +1,9 @@
+from django.db.models import Q
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from webapp.forms import PhotoForm
 from webapp.models import Photo, Album
@@ -12,6 +14,9 @@ class ListPhoto(ListView):
     template_name = "photo/index.html"
     context_object_name = "photos"
     ordering = ('-created_at',)
+
+    def get_queryset(self):
+        return Photo.objects.filter(Q(private=False) | Q(album__private=False))
 
 
 class PhotoView(DetailView):
@@ -36,3 +41,22 @@ class PhotoCreateView(CreateView):
         photo.author = authors
         photo.save()
         return super().form_valid(form)
+
+
+class PhotoUpdateView(UpdateView):
+    model = Photo
+    form_class = PhotoForm
+    template_name = 'photo/update.html'
+    context_object_name = 'photo'
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+
+class PhotoDeleteView(DeleteView):
+    model = Photo
+    template_name = 'photo/delete.html'
+    context_object_name = 'photo'
+    success_url = reverse_lazy('webapp:index')
--- webapp/urls.py
+++ webapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 # from webapp.photo.photo import ListProduct
 #
-from webapp.photo import ListPhoto, PhotoView, PhotoCreateView
+from webapp.photo import ListPhoto, PhotoView, PhotoCreateView, PhotoUpdateView, PhotoDeleteView
 
 app_name = 'webapp'
 #
@@ -10,8 +10,8 @@ urlpatterns = [
     path('', ListPhoto.as_view(), name="index"),
     path('photo/create/', PhotoCreateView.as_view(), name="create_photo"),
     path('photo/<int:pk>/', PhotoView.as_view(), name="view_photo"),
-    # path('product/<int:pk>/update', UpdateProduct.as_view(), name="update_product"),
-    # path('product/<int:pk>/delete', DeleteProduct.as_view(), name="delete_product"),
+    path('photo/<int:pk>/update', PhotoUpdateView.as_view(), name="update_photo"),
+    path('photo/<int:pk>/delete', PhotoDeleteView.as_view(), name="delete_photo"),
     # path('product/<int:pk>/add/review', AddReview.as_view(), name="add_review"),
     # path('product/<int:pk>/update/review', UpdateReview.as_view(), name="update_review"),
     # path('product/<int:pk>/delete/review', DeleteReview.as_view(), name="delete_review"),
